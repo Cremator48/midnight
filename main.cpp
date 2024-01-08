1816180adedd52c69751040af9a62500a7086410
Cremator48 <tyurinanton@list.ru>
2024-01-08 21:09:42 +0300

    Освещение в зависимости от расстояния
    Для этого отредактирован фрагментный шейдер и главная программа.

--- main.cpp
+++ main.cpp
@@ -120,6 +120,18 @@ int main()
        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
     };
 
+    glm::vec3 cubePositions[] = {
+    glm::vec3(0.0f,  0.0f,  0.0f),
+    glm::vec3(2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3(2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3(1.3f, -2.0f, -2.5f),
+    glm::vec3(1.5f,  2.0f, -2.5f),
+    glm::vec3(1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
        
     unsigned int VBO, VAO, lightVAO;
 
@@ -171,6 +183,7 @@ int main()
 
 
     unsigned int diffuseMap = loadTexture("../midnight/res/box.png");
+    unsigned int specularMap = loadTexture("../midnight/res/specular_map.png");
 
 
 
@@ -199,13 +212,17 @@ int main()
         ourShader.setVec3("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
         ourShader.setVec3("material.ambient", glm::vec3(1.0f, 0.5f, 0.31f));
         ourShader.setVec3("material.diffuse", glm::vec3(1.0f, 0.5f, 0.31f));
-        ourShader.setVec3("material.specular", glm::vec3(0.5f, 0.5f, 0.5f));
+        ourShader.setInt("material.specular", 1);
         ourShader.setFloat("material.shininess", 32.0f);
         
 
         ourShader.setVec3("light.ambient", glm::vec3(0.2f, 0.2f, 0.2f));
         ourShader.setVec3("light.diffuse", glm::vec3(0.5f, 0.5f, 0.5f));
         ourShader.setVec3("light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
+
+        ourShader.setFloat("light.constant", 1.0f);
+        ourShader.setFloat("light.linear", 0.09f);
+        ourShader.setFloat("light.quadratic", 0.032f);
 
 
         // model
@@ -225,8 +242,22 @@ int main()
         
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, diffuseMap);
+
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, specularMap);
+        
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            ourShader.setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
 
 
--- shader.fs
+++ shader.fs
@@ -2,7 +2,7 @@
 
 struct Material {
     sampler2D diffuse;
-    vec3 specular;
+    sampler2D specular;
     float shininess;
 }; 
 
@@ -12,6 +12,10 @@ struct Light {
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
 };
 
 out vec4 FragColor;
@@ -35,13 +39,19 @@ void main()
 
 
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords).rgb);
             
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * (spec * material.specular); 
+    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb; 
 
+    float distance = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+
+    ambient *= attenuation; 
+    diffuse *= attenuation;
+    specular *= attenuation; 
 
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
