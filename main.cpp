841cf133a5bc875c0cb486b6b1af6d99e3029273
Cremator48 <tyurinanton@list.ru>
2024-09-04 15:54:08 +0300

    Добавил поддержку отрисовки текста
    Но не оптимизировал ещё

--- main.cpp
+++ main.cpp
@@ -16,11 +16,28 @@
 #include "camera.h"
 #include <random>
 
+#include <map>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H  
+
+struct Character {
+	unsigned int TextureID; // ID текстуры глифа
+	glm::ivec2 Size; // размер глифа
+	glm::ivec2 Bearing; // смещение от линии шрифта до верхнего/левого угла глифа
+	unsigned int Advance; // смещение до следующего глифа
+};
+
+unsigned int glyphVAO, glyphVBO;
+std::map<char, Character> Characters;
+
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void RenderText(Shader& shader, std::string text, float x, float y, float scale, glm::vec3 color);
 
 unsigned int loadTexture(char const* path);
 float lerp(float a, float b, float f);
@@ -44,11 +61,11 @@ float exposure = 0.7f;
 
 int power = 1;
 
-bool moveLight = false;
+bool moveLight = true;
 bool inverseNormals = false;
 float highOfLight = 5.0f;
 
-bool autoExposition = true;
+bool autoExposition = false;
 
 float radius = 13.0f;
 
@@ -78,8 +95,6 @@ float far = 100.0f; // test
 int samples = 20; // test
 
 bool shadows = true;
-
-bool reversFloor;
 
 int main()
 {
@@ -171,6 +186,21 @@ int main()
 		glEnableVertexAttribArray(2);
 
 
+		glBindVertexArray(0);
+
+	}
+
+	// для текста glyphVAO и glyphVBO
+	{
+	
+		glGenVertexArrays(1, &glyphVAO);
+		glGenBuffers(1, &glyphVBO);
+		glBindVertexArray(glyphVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, glyphVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
 		glBindVertexArray(0);
 
 	}
@@ -406,12 +436,13 @@ int main()
 	Shader screenShader("../midnight/screenShader.vs", "../midnight/screenShader.fs", NULL); // Шейдер для совмещения ssao-экранной текстуры и источников освещения
 	Shader shaderBlur("../midnight/screenShader.vs", "../midnight/blur.fs", NULL); // Шейдер для размытия
 	Shader shadowShader("../midnight/shaderForShadow.vs", "../midnight/shadow.fs", "../midnight/geometricShadow.gs"); // Шейдер для размытия
+	Shader glyphShader("../midnight/glyph.vs", "../midnight/glyph.fs", NULL); // Шейдер для текста
 
 
-	unsigned int floor = loadTexture("C:\\Users\\Tyurin\\Documents\\GitHub\\res\\floor.png");
+	unsigned int floor = loadTexture("C:\\Users\\Tyuri\\Documents\\GitHub\\res\\floor.png");
 
 	stbi_set_flip_vertically_on_load(true);
-	Model backpack("C:/Users/Tyurin/Documents/GitHub/res/models/backpack/backpack.obj", aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
+	Model backpack("C:/Users/Tyuri/Documents/GitHub/res/models/backpack/backpack.obj", aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
 
 	// Настройка фреймбуфера для отложенного рендеринга
 	unsigned int gBuffer, gPosition, gNormal, gColorSpec, gWorldPosition, gWorldNormal;
@@ -696,6 +727,92 @@ int main()
 	float curentFrameO; // Время текущего кадра
 
 	float sinOfLastExpHith = glfwGetTime();;
+
+	// Загрузка шрифта
+	{
+		// Библиотека FreeType
+		FT_Library ft;
+
+		// Всякий раз, когда возникает ошибка, функции будут возвращать отличное от нуля значение
+		if (FT_Init_FreeType(&ft))
+		{
+			std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+			return -1;
+		}
+
+		// Путь к файлу шрифта
+
+		std::string font_name = "../res/fonts/a_AlternaSw.ttf";
+		if (font_name.empty())
+		{
+			std::cout << "ERROR::FREETYPE: Failed to load font_name" << std::endl;
+			return -1;
+		}
+
+		// Загружаем шрифт в face
+		FT_Face face;
+		if (FT_New_Face(ft, font_name.c_str(), 0, &face)) {
+			std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+			return -1;
+		}
+		else {
+			// Задаем размер для загрузки глифов
+			FT_Set_Pixel_Sizes(face, 0, 48);
+
+			// Отключаем ограничение выравнивания байтов
+			glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+			// Загружаем глифы символов 
+			for (unsigned int c = 0; c < 256; c++)
+			{
+				// Загружаем глиф символа 
+				if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+				{
+					std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+					continue;
+				}
+
+				// Генерируем текстуру
+				unsigned int texture;
+				glGenTextures(1, &texture);
+				glBindTexture(GL_TEXTURE_2D, texture);
+				glTexImage2D(
+					GL_TEXTURE_2D,
+					0,
+					GL_RED,
+					face->glyph->bitmap.width,
+					face->glyph->bitmap.rows,
+					0,
+					GL_RED,
+					GL_UNSIGNED_BYTE,
+					face->glyph->bitmap.buffer
+				);
+
+				// Задаем для текстуры необходимые опции
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+				// Теперь сохраняем символ для последующего использования
+				Character character = {
+					texture,
+					glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+					glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+					static_cast<unsigned int>(face->glyph->advance.x)
+				};
+				Characters.insert(std::pair<char, Character>(c, character));
+			}
+			glBindTexture(GL_TEXTURE_2D, 0);
+		}
+
+		// Освобождаем использованные ресурсы
+		FT_Done_Face(face);
+		FT_Done_FreeType(ft);
+
+	}
+	
+
 
 	// Цикл рендеринга
 	while (!glfwWindowShouldClose(window))
@@ -1099,7 +1216,17 @@ int main()
 				}
 					
 			}
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			glyphShader.use();
+			projection = glm::ortho(0.0f, static_cast<float>(SCR_WIDTH), 0.0f, static_cast<float>(SCR_HEIGHT));
+			glyphShader.setMat4("projection", projection);
 
+
+
+			RenderText(glyphShader, "Урааа, Получилось!!!", 50.0f, 50.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
+			RenderText(glyphShader, "Удивительно..!", static_cast<float>(SCR_WIDTH) - 300.0f, static_cast<float>(SCR_HEIGHT) - 100.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
+			glDisable(GL_BLEND);
 
 		}
 
@@ -1273,4 +1400,55 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 {
 	if (key == GLFW_KEY_G && action == GLFW_PRESS)
 		autoExposition = !autoExposition;
+}
+
+// Рендер строки текста
+void RenderText(Shader& shader, std::string text, float x, float y, float scale, glm::vec3 color)
+{
+	// Активируем соответствующее состояние рендеринга	
+	shader.use();
+	glUniform3f(glGetUniformLocation(shader.ID, "textColor"), color.x, color.y, color.z);
+	glActiveTexture(GL_TEXTURE0);
+	glBindVertexArray(glyphVAO);
+
+	// Перебираем все символы
+	std::string::const_iterator c;
+	for (c = text.begin(); c != text.end(); c++)
+	{
+		Character ch = Characters[*c];
+
+		float xpos = x + ch.Bearing.x * scale;
+		float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+		float w = ch.Size.x * scale;
+		float h = ch.Size.y * scale;
+
+		// Обновляем VBO для каждого символа
+		float vertices[6][4] = {
+			{ xpos,     ypos + h,   0.0f, 0.0f },
+			{ xpos,     ypos,       0.0f, 1.0f },
+			{ xpos + w, ypos,       1.0f, 1.0f },
+
+			{ xpos,     ypos + h,   0.0f, 0.0f },
+			{ xpos + w, ypos,       1.0f, 1.0f },
+			{ xpos + w, ypos + h,   1.0f, 0.0f }
+		};
+
+		// Рендерим текстуру глифа на прямоугольник
+		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+
+		// Обновляем содержимое памяти VBO
+		glBindBuffer(GL_ARRAY_BUFFER, glyphVBO);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // обязательно используйте glBufferSubData, а не glBufferData
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		// Рендер прямоугольника
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		// Теперь производим смещение для отображения следующего глифа (обратите внимание, что данное смещение измеряется в единицах, составляющих 1/64 пикселя)
+		x += (ch.Advance >> 6) * scale; // битовый сдвиг на 6 разрядов, чтобы получить значение в пикселях (2^6 = 64 (разделите количество 1/64-х пикселей на 64, чтобы получить количество пикселей))
+	}
+	glBindVertexArray(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
 }
